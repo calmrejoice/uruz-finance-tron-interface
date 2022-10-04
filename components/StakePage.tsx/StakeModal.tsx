50d9e2e0fab04de6ebb30e79677ec9cdd848ffac
Sim Tzu Jye <simtj1990@gmail.com>
2022-10-04 23:02:05 +0800

    stake and withdraw modals ui

--- components/StakePage.tsx/StakeModal.tsx
+++ components/StakePage.tsx/StakeModal.tsx
@@ -9,20 +9,50 @@ import {
   Flex,
   Text,
   Button,
+  HStack,
+  Image,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  InputLeftElement,
+  Spacer,
 } from "@chakra-ui/react";
 
 export const StakeModal = ({ isOpen, onClose }: any) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>
+          <HStack>
+            <Image src="/tokens/urz.png" boxSize="30" />
+            <Text>Stake URZ</Text>
+          </HStack>
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Flex>test</Flex>
+          <Flex flexDir="column" my="6">
+            <InputGroup>
+              <InputLeftElement>
+                <Image src="/tokens/urz.png" boxSize="20px" />
+              </InputLeftElement>
+              <Input placeholder="0.00" type="number" variant="filled" />
+              <InputRightElement>
+                <Text as="button" pr="3" fontSize="sm" textDecor="underline">
+                  Max
+                </Text>
+              </InputRightElement>
+            </InputGroup>
+            <HStack my="6">
+              <Image src="/tokens/urz.png" boxSize="20px" />
+              <Text variant="helper">Available URZ</Text>
+              <Spacer />
+              <Text fontWeight="bold">0 URZ</Text>
+            </HStack>
+            <Button>Stake</Button>
+          </Flex>
         </ModalBody>
-
-        <ModalFooter></ModalFooter>
       </ModalContent>
     </Modal>
   );
--- components/StakePage.tsx/WithdrawModal.tsx
+++ components/StakePage.tsx/WithdrawModal.tsx
@@ -8,20 +8,50 @@ import {
   ModalCloseButton,
   Flex,
   Text,
+  HStack,
+  Image,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Spacer,
+  Button,
 } from "@chakra-ui/react";
 
 export const WithdrawModal = ({ isOpen, onClose }: any) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>
+          <HStack>
+            <Image src="/tokens/urz.png" boxSize="30" />
+            <Text>Withdraw URZ</Text>
+          </HStack>
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Flex>test</Flex>
+          <Flex flexDir="column" my="6">
+            <InputGroup>
+              <InputLeftElement>
+                <Image src="/tokens/urz.png" boxSize="20px" />
+              </InputLeftElement>
+              <Input placeholder="0.00" type="number" variant="filled" />
+              <InputRightElement>
+                <Text as="button" pr="3" fontSize="sm" textDecor="underline">
+                  Max
+                </Text>
+              </InputRightElement>
+            </InputGroup>
+            <HStack my="6">
+              <Image src="/tokens/urz.png" boxSize="20px" />
+              <Text variant="helper">Available URZ</Text>
+              <Spacer />
+              <Text fontWeight="bold">0 URZ</Text>
+            </HStack>
+            <Button>Withdraw</Button>
+          </Flex>
         </ModalBody>
-
-        <ModalFooter></ModalFooter>
       </ModalContent>
     </Modal>
   );
